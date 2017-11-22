26da6b43057aae8c55c1c724910d236fa111a9cb
Huy <mailtohuy@gmail.com>
2017-11-22 10:48:01 -0500

    add boilerplate for testing auth,js

---
+++ server/test/test-auth.js
@@ -0,0 +1,11 @@
+const 
+  chai = require('chai'),
+  should = chai.should(),
+  mocha = require('mocha'),
+  auth = require('../auth');
+
+describe('Auth',function(){
+ it('getUser() should not mutate user list');
+ it('authorize() should match paths by root');
+
+});
